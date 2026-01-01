cfe2fa255940ec5855c8790429e4f665678761c7
suvendukungfu <ssuvendukumar489@gmail.com>
2026-01-01 05:42:56 +0530

    Add dark/light mode toggle

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Portfolio Loaded 🚀");
+const toggleBtn = document.getElementById("theme-toggle");
+
+// Load saved theme
+if (localStorage.getItem("theme") === "light") {
+  document.body.classList.add("light");
+  toggleBtn.textContent = "☀️";
+}
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+
+  if (document.body.classList.contains("light")) {
+    localStorage.setItem("theme", "light");
+    toggleBtn.textContent = "☀️";
+  } else {
+    localStorage.setItem("theme", "dark");
+    toggleBtn.textContent = "🌙";
+  }
+});
